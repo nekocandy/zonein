15642c64c5d21739e172a629b1bef14d40bded7f
Aishwaryaa Obla <95568991+codeash24@users.noreply.github.com>
2023-06-02 21:27:19 +0530

    Initial commit

---
+++ app.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import '@unocss/reset/tailwind.css'
+</script>
+
+<template>
+  <div class="h-screen bg-zinc-900 flex items-center justify-center text-white font-mono">
+    Hello, Pizza!
+  </div>
+</template>
