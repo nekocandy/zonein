4963792852499ced31f9e4a1aa72610e77b5a202
Piyush <bhangalepiyush@gmail.com>
2023-06-03 15:06:13 +0530

    feat(hashconnect): keep it here for legacy reasons, revisit later

---
+++ components/Auth/Verify.client.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { HashConnect } from 'hashconnect'
+
+// ref
+const hasExtension = ref(false)
+
+const hashConnect = new HashConnect()
+
+// const initData = await hashConnect.init(appMetadata, 'testnet', true)
+
+// console.log({ initData })
+
+// hashConnect.foundExtensionEvent.once(() => {
+//   console.log('Extension found')
+//   hasExtension.value = true
+// })
+
+async function ConnectWallet() {
+
+}
+</script>
+
+<template>
+  <div>
+    Hello
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ utils/hashconnect.ts
@@ -0,0 +1,8 @@
+import type { HashConnectTypes } from 'hashconnect'
+
+export const appMetadata: HashConnectTypes.AppMetadata = {
+  name: 'ZoneIn',
+  description: 'zoneinto tech',
+  icon: 'https://zonein.tech/favicon.ico',
+  url: 'https://zonein.tech',
+}
