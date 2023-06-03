40405982657a07e525af8d967e8ef6d513ac440d
Piyush <bhangalepiyush@gmail.com>
2023-06-04 01:24:24 +0530

    feat: client side image component

---
+++ components/Post/Image.client.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+const props = defineProps({
+  fileId: String,
+})
+
+const imageId = ref('')
+
+async function getImageData(fileId: string) {
+  const { refresh, data } = await useFetch('/api/content', {
+    query: {
+      fileId,
+    },
+    method: 'GET',
+    server: false,
+    lazy: true,
+  })
+
+  if (!data.value)
+    await refresh()
+
+  const imageElement = document.getElementById(imageId.value)
+  imageElement?.setAttribute('src', data.value?.fileContents || 'no-data-from-hedera')
+  imageElement?.setAttribute('alt', 'function has been invoked')
+}
+
+onMounted(async () => {
+  imageId.value = createId()
+  await getImageData(props.fileId!)
+})
+</script>
+
+<template>
+  <img :id="imageId" class="mx-auto" src="" alt="no, this doesn't load">
+</template>
+
+<style scoped>
+
+</style>
