8e318cd2cfe67762902dcafa28f0b697bf74b5ce
Piyush <bhangalepiyush@gmail.com>
2023-06-03 21:57:25 +0530

    feat: upload file to hedera network

--- components/Post/Upload.vue
+++ components/Post/Upload.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
 const fileUploadElement = ref<HTMLInputElement>()
 const documentType = ref<'photo' | 'certificate'>('photo')
+const caption = ref<string>('')
 const file = ref<File>()
 const base64Image = ref<string | null>()
+const uploadClicked = ref(false)
+const isFileUploading = ref(false)
+const isPostUploading = ref(false)
 
 function toggleDocumentType() {
   documentType.value = documentType.value === 'photo' ? 'certificate' : 'photo'
@@ -16,6 +20,64 @@ function onFileChange(event: any) {
   file.value = event.target.files[0]
 }
 
+async function uploadFile() {
+  if (!base64Image.value)
+    return
+
+  isFileUploading.value = true
+
+  const hederaResponse = await useFetch('/api/upload-image', {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      image: base64Image.value,
+      documentType: documentType.value,
+    }),
+  })
+
+  isFileUploading.value = false
+
+  return {
+    fileId: hederaResponse.data.value!.fileId,
+    transactionId: hederaResponse.data.value!.transactionId,
+  }
+}
+
+async function uploadPost() {
+  const { $toast } = useNuxtApp()
+  uploadClicked.value = true
+  isPostUploading.value = true
+
+  const uploadedData = await uploadFile()
+  if (!uploadedData)
+    return
+
+  const { fileId, transactionId } = uploadedData
+
+  $toast.success(`Created File with ID: ${fileId}`)
+  $toast.success(`Transaction ID: ${transactionId}`)
+
+  const realmApp = useRealmApp()
+
+  const postId = createId()
+  const post = {
+    id: postId,
+    caption: caption.value,
+    image: fileId,
+    type: documentType.value,
+    userId: realmApp.currentUser!.id,
+    createdAt: new Date(),
+    transactionId,
+  }
+
+  const postsCollection = useCollection('posts')
+  await postsCollection.insertOne(post)
+
+  $toast.success('Post uploaded successfully')
+}
+
 watch(file, async (file) => {
   if (file) {
     const base64: string = await toBase64(file) as string
@@ -25,7 +87,7 @@ watch(file, async (file) => {
 </script>
 
 <template>
-  <div v-auto-animate class="h-full w-full flex flex-col gap-4">
+  <div v-auto-animate class="h-full w-full flex flex-col gap-2">
     <div class="bg-[#222c56] rounded-xl flex flex-col gap-6 py-4 px-6 text-white">
       <!-- upload here text -->
       <div class="flex items-center justify-center gap-2">
@@ -58,7 +120,7 @@ watch(file, async (file) => {
 
       <!-- caption -->
       <div class="h-full w-full px-2">
-        <textarea placeholder="// Enter caption here" class="placeholder-text-[#9EA2B0] placeholder:text-sm resize-none w-full p-2 bg-[#37488c] rounded-md focus:outline-none" />
+        <textarea v-model="caption" placeholder="// Enter caption here" class="placeholder-text-[#9EA2B0] placeholder:text-sm resize-none w-full p-2 bg-[#37488c] rounded-md focus:outline-none" />
       </div>
 
       <!-- upload button -->
@@ -76,10 +138,24 @@ watch(file, async (file) => {
       <img :src="base64Image" :alt="base64Image">
     </div>
 
-    <button v-if="base64Image" class="bg-[#222c56] w-full py-4 text-white rounded-xl">
+    <button v-if="base64Image && !uploadClicked" class="bg-[#222c56] w-full py-4 text-white rounded-xl" @click="uploadPost">
       <Icon class="h-6 w-6" name="tabler:world-upload" />
       Upload & Post
     </button>
+
+    <div v-if="uploadClicked" class="bg-[#222c56] rounded-xl flex flex-col gap-4 py-4 px-6 text-white">
+      <div class="flex items-center gap-2">
+        <Icon v-if="isFileUploading" name="tabler:circle-dotted" class="h-6 w-6 animate-spin" />
+        <Icon v-else name="tabler:circle-check" class="h-6 w-6" />
+        <span class="text-sm">Uploading File</span>
+      </div>
+
+      <div class="flex items-center gap-2">
+        <Icon v-if="isFileUploading" name="tabler:circle-dotted" class="h-6 w-6 animate-spin" />
+        <Icon v-else name="tabler:circle-check" class="h-6 w-6" />
+        <span class="text-sm">Uploading File & Post</span>
+      </div>
+    </div>
   </div>
 </template>
 
