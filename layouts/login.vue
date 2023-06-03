44f727099f9cbcf6a154c88963eca47b0ea37e4d
Piyush <bhangalepiyush@gmail.com>
2023-06-03 10:34:48 +0530

    feat(layout): login page layout

---
+++ layouts/login.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="bg-[#01071f] bg-center bg-no-repeat bg-[url(https://cdn.discordapp.com/attachments/1114413644961292368/1114413658420809768/Loign_BG.png)]">
+    <div class="h-screen">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
