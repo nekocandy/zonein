048cbb19a2496e6f496c97ead14f32edd45828de
Piyush <bhangalepiyush@gmail.com>
2023-06-03 10:35:07 +0530

    feat(app): use layout and page

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <div class="h-screen bg-zinc-900 flex items-center justify-center text-white font-mono">
-    Hello, Pizza!
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
