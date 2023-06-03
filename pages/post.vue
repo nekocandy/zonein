9487c62c25e5a4ecd5806890cfc7d72770d7eeee
Piyush <bhangalepiyush@gmail.com>
2023-06-03 19:30:57 +0530

    feat: post page

---
+++ pages/post.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="h-full w-full">
+    <div class="h-full w-full grid grid-cols-12 gap-6">
+      <div class="col-span-3">
+        <PostUpload />
+      </div>
+
+      <div />
+
+      <div />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
