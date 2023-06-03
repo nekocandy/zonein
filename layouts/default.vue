fde3f1178b264402c4d126bacfda3af3512d0641
Piyush <bhangalepiyush@gmail.com>
2023-06-04 00:16:11 +0530

    fix(layouts/default): disable overflow

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,7 @@
   <div class="bg-[#9c9dc8]">
     <div class="h-screen flex flex-col">
       <Navbar />
-      <div class="max-w-7xl mx-auto px-2 py-4 flex-1 w-full">
+      <div class="max-w-7xl mx-auto px-2 py-4 flex-1 w-full overflow-y-hidden">
         <slot />
       </div>
     </div>
