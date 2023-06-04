5ea649732c65b4f766abf77b6e8ee6f131ae6d16
Piyush <bhangalepiyush@gmail.com>
2023-06-04 16:40:33 +0530

    feat: basic chat page

---
+++ pages/chat.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const message = ref('')
+</script>
+
+<template>
+  <div class="h-full w-full">
+    <div class="h-full w-full grid grid-cols-12 gap-6">
+      <div class="col-span-3 overflow-y-auto" />
+
+      <div class="col-span-6 w-full h-full bg-[#222c56] p-6 flex flex-col gap-4">
+        <div class="bg-white rounded-xl h-full">
+          /
+        </div>
+
+        <div class="flex items-center gap-4">
+          <input v-model="message" class="flex-1 w-full bg-white text-black rounded-xl py-2 px-4" placeholder="// enter message" type="text">
+          <button>
+            <Icon name="tabler:send" class="h-6 w-6" />
+          </button>
+        </div>
+      </div>
+
+      <div class="col-span-3" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
